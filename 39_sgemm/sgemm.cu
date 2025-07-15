4317e7726845dd24443e6c3b1949cf50591feb5c
wangzy0327 <wangzy0327@qq.com>
2025-07-15 03:37:12 +0000

    add 64x64 tile

--- 39_sgemm/sgemm.cu
+++ 39_sgemm/sgemm.cu
@@ -514,6 +514,119 @@ void mysgemm_v6(int M, int N, int K, float alpha, const float* A, const float* B
     *(float4 *)(&(C(row1,col))) = Cres;
 }
 
+#define A(i,j) A[(i) + (j)*lda]
+#define B(i,j) B[(i) + (j)*ldb]
+#define C(i,j) C[(i) + (j)*ldc]
+#define sa7(i,j) sa7[((j)<<6) + (i)]
+#define sb7(i,j) sb7[(((j)<<6)) + (i)]
+#define MS_7 64
+#define NS_7 64
+#define KS_7 64
+__global__  __launch_bounds__(256)
+void mysgemm_v7(int M, int N, int K, float alpha, const float* A, const float* B, float beta, float* C){
+    int lda = M, ldb = K, ldc = M;
+    //获取每个block处理的数据块
+    int tx = threadIdx.x;
+    int bx = blockIdx.x, by = blockIdx.y;
+    A = &A(bx<<6,0);
+    B = &B(0,by<<6);
+    C = &C(bx<<6,by<<6);
+    //64x64/256 per thread handle 16 data split
+    __shared__ float sa7[KS_7*MS_7];
+    __shared__ float sb7[KS_7*NS_7];
+    int row = tx%64;  // [0,1,2,3,...,63]
+    int col = tx/64;  // [0,1,2,3] -> *16+i
+    int loop = 16;
+    float tmp[4][4] = {0.};
+    //循环展开
+    int A_idx[4]; // 0_idx [0,1] [32,33] ... 1_idx [2,3] [34,35] ... 16_idx [30,31] [62,63]
+    int B_idx[4]; // 0_idx [0,1] [32,33] ... 1_idx [2,3] [34,35] ... 16_idx [30,31] [62,63]
+    A_idx[0] = tx%16*2; A_idx[1] = A_idx[0]+1; A_idx[2] = A_idx[0]+32; A_idx[3] = A_idx[0]+33;
+    B_idx[0] = tx/16*2; B_idx[1] = B_idx[0]+1; B_idx[2] = B_idx[0]+32; B_idx[3] = B_idx[0]+33;
+    for(int k_count = 0;k_count < K;k_count+=KS_7){
+        #pragma unroll
+        for(int i = 0;i < loop;i++){
+            // if(i == 0){
+            //     printf("Matrix A (iy:ix) = (%d,%d) handle (%d,%d)  \n",row,col,row,col);
+            //     printf("Matrix B (iy:ix) = (%d,%d) handle (%d,%d) transpose to (%d,%d) \n",row,col,row,(row+col+i)%NS_7,(row+col+i)%NS_7,row);
+            // }
+            sa7(row,col*loop+i) = A(row,col*loop+i);
+            sb7((row+col*loop+i)%NS_7,row) = B(row,(row+col*loop+i)%NS_7);
+        }
+        A += (lda<<6); B += 64;
+        __syncthreads();
+        #pragma unroll
+        for(int inner_k = 0;inner_k < KS_7;inner_k++){
+            for(int i = 0;i < 4;i++){
+                for(int j = 0;j < 4;j++){
+                    tmp[i][j] += sa7(A_idx[i],inner_k) * sb7(B_idx[j],inner_k);
+                }
+            }
+        }
+        __syncthreads();
+    }
+    #pragma unroll
+    for(int i = 0;i < 4;i++){
+        for(int j = 0;j < 4;j++){
+            C(A_idx[i],B_idx[j]) = alpha * tmp[i][j] + beta * C(A_idx[i], B_idx[j]);
+        }
+    }
+}
+
+__global__  __launch_bounds__(256)
+void mysgemm_v7_plus(int M, int N, int K, float alpha, const float* A, const float* B, float beta, float* C){
+    int lda = M, ldb = K, ldc = M;
+    //获取每个block处理的数据块
+    int tx = threadIdx.x;
+    int bx = blockIdx.x, by = blockIdx.y;
+    A = &A(bx<<6,0);
+    B = &B(0,by<<6);
+    C = &C(bx<<6,by<<6);
+    //64x64/256 per thread handle 16 data split
+    __shared__ float sa7[KS_7*MS_7];
+    __shared__ float sb7[KS_7*NS_7];
+    int row = tx%64;  // [0,1,2,3,...,63]
+    int col = tx/64;  // [0,1,2,3] -> *16+i
+    int loop = 16;
+    float2 tmp[4][2] = {0.};
+    //循环展开
+    int A_idx[4]; // 0_idx [0,1] [32,33] ... 1_idx [2,3] [34,35] ... 16_idx [30,31] [62,63]
+    int B_idx[4]; // 0_idx [0,1] [32,33] ... 1_idx [2,3] [34,35] ... 16_idx [30,31] [62,63]
+    A_idx[0] = tx%16*2; A_idx[1] = A_idx[0]+1; A_idx[2] = A_idx[0]+32; A_idx[3] = A_idx[0]+33;
+    B_idx[0] = tx/16*2; B_idx[1] = B_idx[0]+1; B_idx[2] = B_idx[0]+32; B_idx[3] = B_idx[0]+33;
+    for(int k_count = 0;k_count < K;k_count+=KS_7){
+        #pragma unroll
+        for(int i = 0;i < loop;i++){
+            // if(i == 0){
+            //     printf("Matrix A (iy:ix) = (%d,%d) handle (%d,%d)  \n",row,col,row,col);
+            //     printf("Matrix B (iy:ix) = (%d,%d) handle (%d,%d) transpose to (%d,%d) \n",row,col,row,(row+col+i)%NS_7,(row+col+i)%NS_7,row);
+            // }
+            sa7(row,col*loop+i) = A(row,col*loop+i);
+            sb7((row+col*loop+i)%NS_7,row) = B(row,(row+col*loop+i)%NS_7);
+        }
+        A += (lda<<6); B += 64;
+        __syncthreads();
+        #pragma unroll
+        for(int inner_k = 0;inner_k < KS_7;inner_k++){
+            for(int i = 0;i < 4;i++){
+                for(int j = 0;j < 4;j+=2){
+                    tmp[i][j/2].x += sa7(A_idx[i],inner_k) * sb7(B_idx[j],inner_k);
+                    tmp[i][j/2].y += sa7(A_idx[i],inner_k) * sb7(B_idx[j+1],inner_k);
+                }
+            }
+        }
+        __syncthreads();
+    }
+    #pragma unroll
+    for(int i = 0;i < 4;i++){
+        for(int j = 0;j < 4;j+=2){
+            C(A_idx[i],B_idx[j]) = alpha * tmp[i][j/2].x + beta * C(A_idx[i], B_idx[j]);
+            C(A_idx[i],B_idx[j+1]) = alpha * tmp[i][j/2].y + beta * C(A_idx[i], B_idx[j]);
+        }
+    }
+}
+
+
 
 #define sa7(i,j) sa6[(((j)<<6)) + (i)]
 #define sb7(i,j) sb6[(((j)<<6)) + (i)]
@@ -625,7 +738,7 @@ void mysgemm_v7_ano(int M, int N, int K, float alpha, const float* A, const floa
 // more workloads per thread. 4x4 micro kernel.
 // adopt vetorized load/store
 __global__  __launch_bounds__(256)
-void mysgemm_v7(int M, int N, int K, float alpha, const float* A, const float* B, float beta, float* C){
+void mysgemm_v7_pro(int M, int N, int K, float alpha, const float* A, const float* B, float beta, float* C){
     int lda = M, ldb = K, ldc = M;
     int tx = threadIdx.x;
     int bx = blockIdx.x, by = blockIdx.y;
@@ -762,7 +875,7 @@ int main(int argc,char **argv)
   }
   int m, n, k,max_size;
   int N=1, upper_limit;
-  if (kernel<=6&&kernel!=0) upper_limit=8;
+  if (kernel<=7&&kernel!=0) upper_limit=8;
   else upper_limit=(sizeof(SIZE)/sizeof(int));
   max_size=SIZE[upper_limit-1];
   float* A_host = NULL,*B_host = NULL, *C_host = NULL,*C_from_dev = NULL,*C_from_dev_lib = NULL;
@@ -848,6 +961,7 @@ int main(int argc,char **argv)
         case 4: test_mysemm_v4(m,n,k,alpha,A_dev,B_dev,beta,C_dev);break;
         case 5: test_mysemm_v5(m,n,k,alpha,A_dev,B_dev,beta,C_dev);break;
         case 6: test_mysemm_v6(m,n,k,alpha,A_dev,B_dev,beta,C_dev);break;
+        case 7: test_mysemm_v7(m,n,k,alpha,A_dev,B_dev,beta,C_dev);break;
         default:
           break;
       }
